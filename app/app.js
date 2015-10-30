cb25fc2a1248fb45d6691097c70b27f1cddefc19
tasar <tasar.1990@gmail.com>
2015-10-30 11:12:30 +0530

    hh

--- app/app.js
+++ app/app.js
@@ -25,45 +25,55 @@ app.controller('listCtrl', function ($scope, services) {
     $scope.alltimings={};
     $scope.moviedetail = {};
     $scope.searchData = function(alltimings){
+    	var show_time = alltimings.selectone;
+    	var booking_date = alltimings.selectdate;
+    	
     	services.getmovies(alltimings).then(function(data){
     		//console.log(data);
             $scope.moviedetail = data.data;
         });
+    
+	    $scope.showdetail_seat = function(seatid,selectdate){
+	    	services.getallseats().then(function(data){
+	            $scope.allseats = data.data;
+	        });
+	    	
+	    	var search_data = {'seatid': seatid, 'selectdate': selectdate};
+	    	services.getshowseats(search_data).then(function(data){
+	            $scope.seatdetail = {axx: data.data};
+	    		//console.log($scope.seatdetail);
+	            $scope.isExist = function(id){
+	    	        return $scope.seatdetail.axx.map(function(type){
+	    	        	return type.seat_no;
+	    	        }).indexOf(id);
+	    	    }
+	        }); 
+	    	//$scope.items = [];
+	    	$scope.items={ seatPlaceholder: [],seatId: [],namefields: [],emailfields: []  };
+	    	
+	    	$scope.addSelection = function($event, id, seatname){
+	    		var checkbox = $event.target;
+	    		var action = (checkbox.checked ? 'add' : 'remove');
+	    		
+	              $scope.items.seatPlaceholder.push(seatname);
+	              $scope.items.seatId.push(id);
+	              $scope.items.namefields.push('');
+	              $scope.items.emailfields.push('');
+	              //console.log($scope.items);
+	    	};
+	    	
+	    	$scope.sellTickit= function(items){
+//	        	console.log(items);
+//	        	console.log(show_time);
+//	        	console.log(booking_date);
+//	        	console.log(search_data);
+	    		angular.forEach(items, function(value, key) {
+	    			console.log(key + ': ' + value);
+	    		});
+	        };
+	    };
     };
     
-    $scope.showdetail_seat = function(seatid,selectdate){
-    	services.getallseats().then(function(data){
-            $scope.allseats = data.data;
-        });
-    	
-    	var search_data = {'seatid': seatid, 'selectdate': selectdate};
-    	services.getshowseats(search_data).then(function(data){
-            $scope.seatdetail = {axx: data.data};
-    		//console.log($scope.seatdetail);
-            $scope.isExist = function(id){
-    	        return $scope.seatdetail.axx.map(function(type){
-    	        	return type.seat_no;
-    	        }).indexOf(id);
-    	    }
-        }); 
-    	//$scope.items = [];
-    	$scope.items={ seatPlaceholder: [],seatId: [],namefields: [],emailfields: []  };
-    	
-    	$scope.addSelection = function($event, id, seatname){
-    		var checkbox = $event.target;
-    		var action = (checkbox.checked ? 'add' : 'remove');
-    		
-              $scope.items.seatPlaceholder.push(seatname);
-              $scope.items.seatId.push(id);
-              $scope.items.namefields.push('');
-              $scope.items.emailfields.push('');
-              console.log($scope.items);
-    	};
-    };
-    
-    $scope.sellTickit= function(items){
-    	console.log(items);
-    };
 });
 
 app.config(['$routeProvider',
